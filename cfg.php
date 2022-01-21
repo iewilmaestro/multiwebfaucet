c86d333819a46327d104bd81e9daa29cfc8c3767
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2022-01-21 22:00:30 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,24 @@
+<?php
+/*
+*
+*Creator	: iewil
+*Github		: https://github.com/iewilmaestro
+*Youtube	: https://www.youtube.com/c/iewil
+*Telegram	: @iewil57
+*Support	: Team-Function-INDO
+*
+*/
+$user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
+/*
+@Cookie
+*/
+$mcm = "xxx";
+$dragon = "_ga=GA1.2.659934884.1641875811; session_new=purna.iera@gmail.com; session_new_id=ccb0bde58eb1a86ce9c9f8d6c2f34aa5; _pbjs_userid_consent_data=3524755945110770; HstCfa4531111=1641875931131; HstCmu4531111=1641875931131; __dtsu=51A016418759290C221C93A1391ED7F8; _cc_id=ca2a8dd5148851fbf2d397321a758840; c_ref_4531111=https%3A%2F%2Fphoenixfaucets.xyz%2F; _gid=GA1.2.1266323765.1642513621; cto_bundle=AijIB19yOGZZNWdwWVdsVEphU1RhcWhFeEVCaU56bk4xZFNXMXR3aHY2Nm11czhLZzhGaSUyQnZLNVNuUUhSVWpmTSUyQmpkJTJGUFQ2RW9UJTJCVktmVVhYUzVvcU4wSHZBVyUyRm9WR1ZKaXhMdUNMS2klMkJSMWFoc2YwalBVRHhpNml4d0NQRGxuNHFWNEJtJTJCNFJoRDRENHBFMFh0T2ZXUGwxQSUzRCUzRA; cto_bidid=3kvhEl9vczV3SU1qWmRZRmZZYkdxblZNeTFqTXROazllb1h1TjZYQSUyRlZBcEk0Q0IlMkYydzdDNDFwck5ZR3lyb2FPemZYam1BajI0bXFoQXJ3SDJCSmtPViUyQlN4aGJ2aXlkdUhGcjI3Ym9YV2o4NDJTT2t0Qk5oa3ZsOUdJUnIlMkY3VUpXZUFK; session_ok=true; __viCookieActive=true; __cf_bm=E_93uNbRbrc8rvGIEseeOYrw6aNsDD1bnp50wiG4LYk-1642775715-0-AUoMVoWJDTwwfvCOUn+GhgfufTPAaqVp8tC215a2uMMMqotcOJ7GUQpKPi31ameVCA==; auto={%22email%22:%22purna.iera@gmail.com%22%2C%22coins%22:[%22doge%22%2C%22ltc%22%2C%22trx%22%2C%22FEY%22%2C%22SOL%22]%2C%22mode%22:%22multi%22%2C%22boost%22:%221%22%2C%22payout_mode%22:%22balance%22}; cf_chl_2=feb275eca4ebca9; cf_chl_prog=x11; cf_clearance=svGQIShuP1BrXA1DEGPVl3pmKXSGZaGw9xBhtWlLzyU-1642775735-0-150; HstCnv4531111=7; HstCns4531111=7; _data_cpm=129-1; panoramaId_expiry=1642862147761; HstCla4531111=1642775862258; HstPn4531111=6; HstPt4531111=588; _gat_gtag_UA_179106564_3=1; _gat_gtag_UA_70132428_1=1; _data_cpc=69-1_77-2; _data_html=55-1_109-1_129-1_136-1_646-1_647-1_648-1_661-1_662-1; cto_bundle=mxhcVF9yOGZZNWdwWVdsVEphU1RhcWhFeEVOTDM4bFZaMjdIajJySjJ1bSUyQjF1VThrMjZCS0dqYnBheFlaNGdXMTdLYm1WZFBOcDFreERyMzJlYWdYekdCaERFZ1ZoZnBObllrUXdKY0dleUJCWFcxUDBONEJaJTJCJTJCUDJTUVNCcFlUQ09IR2NnUUJnZmRxZnlvU1lMdnF6UyUyRlh2USUzRCUzRA; cto_bidid=AEOQWV9vczV3SU1qWmRZRmZZYkdxblZNeTFqTXROazllb1h1TjZYQSUyRlZBcEk0Q0IlMkYydzdDNDFwck5ZR3lyb2FPemZYam1BajI0bXFoQXJ3SDJCSmtPViUyQlN4aGJ2aXlkdUhGcjI3Ym9YV2o4NDJTT3Zic0ZJNSUyQmtKb015ZU16T1lrQXQ5";
+$best = "xxx";
+$phoenix = "xxx";
+$cryptos = "xxx";
+$paybits = "xxx";
+$xtra = "xxx";
+$alpha = "xxx";
+$jupi = "xxx";
+
